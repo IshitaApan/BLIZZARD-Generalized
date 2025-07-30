b7f7a027c643450e5b1f48ee01ff927867cd61d7
IshitaApan <bsse0922@iit.du.ac.bd>
2025-07-30 16:31:09 +0600

    Creating STACK_TRACE for bug reports of ST group if  not exist for a project

--- src/blizzard/bug/report/classification/TraceLoader.java
+++ src/blizzard/bug/report/classification/TraceLoader.java
@@ -1,23 +1,45 @@
 package blizzard.bug.report.classification;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import blizzard.config.StaticData;
+import blizzard.utility.BugReportLoader;
 import blizzard.utility.ContentLoader;
 
 public class TraceLoader {
 
 	public static ArrayList<String> loadStackTraces(String repoName, int bugID) {
 		ArrayList<String> straces = new ArrayList<>();
-		try {
+
 			String traceFile = StaticData.STACK_TRACE_DIR + "/" + repoName + "/" + bugID + ".txt";
 			File tFile = new File(traceFile);
 			if (tFile.exists()) {
-				straces = ContentLoader.getAllLinesList(traceFile);
+				try {
+					// Load the stack traces from the file
+					straces = ContentLoader.getAllLinesList(traceFile);
+				} catch (Exception e) {
+					System.err.println("Error reading stack trace file: " + e.getMessage());
+				}
 			}
-		} catch (Exception exc) {
-			System.err.println("Failed to load the stack traces!" + exc.getMessage());
-		}
+			else {
+				System.err.println("No stack trace file found for " + repoName + " bug ID: " + bugID);
+				
+				String bugReport = BugReportLoader.loadBugReport(repoName, bugID);
+				TraceCollector traceCollector = new TraceCollector(null);
+				straces = traceCollector.extractStackTraces(bugReport);
+				// write code to Save the extracted stack traces to a file
+				try (BufferedWriter writer = new BufferedWriter(new FileWriter(tFile))) {
+					for (String entry : straces) {
+						writer.write(entry);
+						writer.newLine();
+					}
+				} catch (Exception e) {
+					System.err.println("Error writing to file: " + e.getMessage());
+				}
+				System.err.println("Created stack trace file: " + StaticData.STACK_TRACE_DIR + "/" + repoName + "/" + bugID + ".txt");
+			}
 		return straces;
 	}
 
