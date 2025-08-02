738c12ad750eba82c410cc95243cb8bc09b9c15d
IshitaApan <bsse0922@iit.du.ac.bd>
2025-08-02 09:26:29 +0600

    testing stack trace

--- src/blizzard/tests/TraceCollectorTest.java
+++ src/blizzard/tests/TraceCollectorTest.java
@@ -1,7 +1,14 @@
 package blizzard.tests;
 
 import static org.junit.Assert.assertNotNull;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.junit.Test;
 import blizzard.bug.report.classification.TraceCollector;
 import blizzard.bug.report.classification.TraceLoader;
@@ -19,6 +26,51 @@ public class TraceCollectorTest {
 		ArrayList<String> entries = traceCollector.extractStackTraces(bugReport);
 		System.out.println(entries);
 		assertNotNull(entries);
+	}
+
+	@Test
+	public void testCollectTracesFromAspectj() {
+		String repoName = "birt";
+		int bugID = 103106; // birt: 103106 // aspectj: 220172
+		String bugReport = BugReportLoader.loadBugReport(repoName, bugID);
+
+		ArrayList<String> traces = new ArrayList<>();
+		String stackRegexBlizzard = "(.*)?(.+)\\.(.+)(\\((.+)\\.java:\\d+\\)|\\(Unknown Source\\)|\\(Native Method\\))";
+		//String stackRegexAltered = "([A-Za-z0-9_\\.$<>]+)\\((?:[A-Za-z0-9_]+\\.java:\\d+|Unknown Source|Native Method)\\)";
+		
+		Pattern p = Pattern.compile(stackRegexBlizzard);
+		Matcher m = p.matcher(bugReport);
+		while (m.find()) {
+			String entry = bugReport.substring(m.start(), m.end());
+			entry = cleanTheEntry(entry);
+			traces.add(entry);
+		}
+
+		String traceFile = "src/blizzard/tests/" + repoName + "/" + bugID + "_Blizzard.txt";
+		File file = new File(traceFile);
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+			for (String entry : traces) {
+				writer.write(entry);
+				writer.newLine();
+			}
+		} catch (Exception e) {
+			System.err.println("Error writing to file: " + e.getMessage());
+		}
+
+		System.out.println(traces);
+		assertNotNull(traces);
+	}
+
+	protected String cleanTheEntry(String entry) {
+		if (entry.indexOf("at ") >= 0) {
+			int atIndex = entry.indexOf("at");
+			entry = entry.substring(atIndex + 2).trim();
+		}
+		if (entry.contains("(")) {
+			int leftBraceIndex = entry.indexOf("(");
+			entry = entry.substring(0, leftBraceIndex);
+		}
+		return entry;
 	}
 
 	@Test
