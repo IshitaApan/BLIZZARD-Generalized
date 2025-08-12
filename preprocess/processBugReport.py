73c2c1d39ee8001a6d9b8219a81cd14cc5a77a62
IshitaApan <bsse0922@iit.du.ac.bd>
2025-08-12 13:20:24 +0600

    process  raw query and inputs for all project under a folder

--- preprocess/processBugReport.py
+++ preprocess/processBugReport.py
@@ -41,7 +41,7 @@ def dump_bug_reports(xml_path, output_dir, bugIds_file):
         for bid in bug_ids:
             f.write(f"{bid}\n")
 
-if __name__ == "__main__":
+def process_projectwise_bug_report() :
     project_name = "tomcat"  # replace with your project name, [aspectj, birt, eclipse, jdt, swt, tomcat]
     xml_file = f"/home/ishita/BugLocalization/Data-22k/{project_name}-updated-data.xml"         # replace with your XML path
     output_folder = f"/home/ishita/BugLocalization/Data-22k/BR-RAW-Query/{project_name}"    # directory to hold the .txt files
@@ -49,3 +49,39 @@ if __name__ == "__main__":
     dump_bug_reports(xml_file, output_folder, bugIds_file)
     print(f"Processed {len(os.listdir(output_folder))} reports; bug_ids written.") 
     print(f"Written all bug files to ./{output_folder}/")
+
+def process_all_project_bug_reports(folder_path):
+    """
+    Iterates through a folder, finds all XML files, and process bug report from the related project
+    Args:
+        folder_path (str): The path to the folder to be scanned.
+    """
+    project_names = []
+    try:
+        # Get a list of all items (files and folders) in the specified directory
+        for item in os.listdir(folder_path):
+            item_path = os.path.join(folder_path, item)
+            # Check if the item is a file and ends with '.xml'
+            if os.path.isfile(item_path) and item.endswith('.xml'):
+                # Split the filename and its extension, then add the filename
+                file_name_without_ext, _ = os.path.splitext(item)
+                project_names.append(file_name_without_ext)
+        
+        # Print the entire list of files found
+        print(f"Found {len(project_names)} XML files (without extensions):")
+        for project_name in project_names:
+            print(f"****{project_name}****")
+            xml_file = f"{folder_path}/{project_name}.xml"
+            output_folder = f"BR-Raw/GHRB/{project_name}"    # directory to hold the .txt files
+            bugIds_file = f"inputs/GHRB/{project_name}_bug_ids.txt"  # file to hold the bug IDs
+            dump_bug_reports(xml_file, output_folder, bugIds_file)
+            print(f"Processed {len(os.listdir(output_folder))} reports; bug_ids written.") 
+            print(f"Written all bug files to ./{output_folder}/")
+
+    # except FileNotFoundError:
+    #     print(f"Error: The folder '{folder_path}' was not found.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+if __name__ == "__main__":
+    process_all_project_bug_reports("/home/ishita/BugLocalization/GHRB")
